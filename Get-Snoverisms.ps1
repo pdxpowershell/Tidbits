325056f5f7f3a00cfcaef41e0e3613c5d4fe5d37
pdxpowershell <pdxpowershell@gmail.com>
2016-06-17 02:00:35 +0000

    Simple BitsTransfer
    In honor of PowerShell Creator @jsnover #powershell

---
+++ Get-Snoverisms.ps1
@@ -0,0 +1,5 @@
+﻿$Snoverisms = Invoke-WebRequest -Uri http://snoverisms.com/ | 
+Select -ExpandProperty images | 
+Select -ExpandProperty src |
+foreach { start-bitstransfer $("http://snoverisms.com/{0}" -f $_) $ENV:PUBLIC\Downloads}
+
